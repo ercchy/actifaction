d7fe386f4d281f678ae19f1388867fd61f655f2c
Lena Hofman <lena.hofman@gmail.com>
2013-11-20 21:09:30 +0100

    Added Action in models

--- actifaction/api/models.py
+++ actifaction/api/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class Action(models.Model):
+	creation_date = models.DateField()
+	title = models.CharField(max_length=255)
+	location = models.CharField(max_length=255)
+	description = models.TextField(max_length=255)
+	action_date = models.DateTimeField()
+	action_type = models.IntegerField()
