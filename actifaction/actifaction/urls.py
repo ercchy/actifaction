284be3febafa1233f4b04a89075d0d34d6f1b293
Mateja Verlic <mateja.verlic@gmail.com>
2013-12-06 12:55:22 +0100

    Fixed settings

--- actifaction/actifaction/urls.py
+++ actifaction/actifaction/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from actifaction import settings
+from django.conf import settings
 
 admin.autodiscover()
 
