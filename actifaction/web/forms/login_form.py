f32b537eb7d80d97bb5e6debf6aa0582ec0bb8dd
ercchy <erika@serox.si>
2013-12-10 20:30:51 +0100

    reformatting login form to use HTML that comes with crispy forms

--- actifaction/web/forms/login_form.py
+++ actifaction/web/forms/login_form.py
@@ -1,13 +1,26 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import ugettext as _
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit, Fieldset, ButtonHolder, Layout
 
 
 class LoginForm(AuthenticationForm):
 	def __init__(self, *args, **kwargs):
 		super(LoginForm, self).__init__(*args, **kwargs)
 
+		layout = Layout(
+			Fieldset(_('Prijava'),
+			         'username',
+			         'password',
+			         ),
+			ButtonHolder(
+				Submit('submit', _('Prijava'), css_class='btn btn-lg btn-primary btn-block'),
+			),
+		)
+
 		self.helper = FormHelper()
 		self.helper.form_method = 'POST'
-		# Translators: This is for login page only
-		self.helper.add_input(Submit('login','Login', css_class='btn-primary'))
+		self.helper.form_tag = True
+		self.helper.form_class = 'form-signin'
+		self.helper.add_layout(layout)
+
