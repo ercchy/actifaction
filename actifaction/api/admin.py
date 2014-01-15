156a8fed286130a3626b6e0911bab24fa32218e8
ercchy <erika@serox.si>
2014-01-15 15:37:52 +0100

    changed the listing of user profile in admin

--- actifaction/api/admin.py
+++ actifaction/api/admin.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 import models
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', )
+
+
 admin.site.register(models.ActionCategory)
 admin.site.register(models.Action)
-admin.site.register(models.UserProfile)
+admin.site.register(models.UserProfile, UserProfileAdmin)
